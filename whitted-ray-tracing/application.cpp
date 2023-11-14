471c74af35c2f047c2e4c75fe7c3fd1c0ffd979f
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-11-14 11:22:41 +0100

    Got some intersection

--- whitted-ray-tracing/application.cpp
+++ whitted-ray-tracing/application.cpp
@@ -22,91 +22,13 @@
         auto a = 0.5 * (unit_direction.y + 1.0);
         return (1.0 - a) * colorRGB(1.0, 1.0, 1.0) + a * colorRGB(0.5, 0.7, 1.0);
     }
-
-    // The intersection algorithm using the "slab method"
-    bool intersectBox(const ray & ray, const glm::vec3 & min, const glm::vec3 & max, HitInfo & info) {
-        // Initialize variables for the minimum and maximum intersection distances along the ray
-        float tXMin, tXMax, tYMin, tYMax, tZMin, tZMax;
-
-        tXMin = (min.x - ray.origin().x) / ray.direction().x;
-        tXMax = (max.x - ray.origin().x) / ray.direction().x;
-
-        // Swap tXMin and tXMax if necessary to ensure tXMin <= tXMax
-        if (tXMin > tXMax) {
-            std::swap(tXMin, tXMax);
-        }
-
-        // Calculate intersection distances along the Y-axis
-        tYMin = (min.y - ray.origin().y) / ray.direction().y;
-        tYMax = (max.y - ray.origin().y) / ray.direction().y;
-
-        // Swap tYMin and tYMax if necessary
-        if (tYMin > tYMax) {
-            std::swap(tYMin, tYMax);
-        }
-
-        // Check if the intersection along the Y-axis overlaps with the X-axis intersection
-        if ((tXMin > tYMax) || (tYMin > tXMax)) {
-            info.hit = false; // No intersection
-            return false;
-        }
-
-        // Update tXMin and tXMax to consider the Y-axis intersections
-        tXMin = std::max(tXMin, tYMin);
-        tXMax = std::min(tXMax, tYMax);
-
-        // Calculate intersection distances along the Z-axis
-        tZMin = (min.z - ray.origin().z) / ray.direction().z;
-        tZMax = (max.z - ray.origin().z) / ray.direction().z;
-
-        // Swap tZMin and tZMax if necessary
-        if (tZMin > tZMax) {
-            std::swap(tZMin, tZMax);
-        }
-
-        // Check if the intersection along the Z-axis overlaps with the updated X and Y-axis intersections
-        if ((tXMin > tZMax) || (tZMin > tXMax)) {
-            info.hit = false; // No intersection
-            return false;
-        }
-
-        // Update tXMin and tXMax to consider the Z-axis intersections
-        tXMin = std::max(tXMin, tZMin);
-        tXMax = std::min(tXMax, tZMax);
-
-        // Check if the intersection is in the positive ray direction
-        if (tXMin < 0 && tXMax < 0) {
-            info.hit = false; // Intersection behind the ray's origin
-            return false;
-        }
-
-        // If tXMin is positive, it's the entry point of intersection
-        // Calculate the hit point and normal at the intersection
-        info.hit = true;
-        info.t = (tXMin >= 0) ? tXMin : tXMax; // Use the nearest intersection point
-        info.hitPoint = ray.origin() + info.t * ray.direction();
-
-        // Calculate the normal of the hit face (outward-facing normal of the AABB)
-        if (info.t == tXMin) {
-            info.normal = glm::vec3(-1.0f, 0.0f, 0.0f); // Left face
-        }
-        else if (info.t == tXMax) {
-            info.normal = glm::vec3(1.0f, 0.0f, 0.0f); // Right face
-        }
-        else if (info.t == tYMin) {
-            info.normal = glm::vec3(0.0f, -1.0f, 0.0f); // Bottom face
-        }
-        else if (info.t == tYMax) {
-            info.normal = glm::vec3(0.0f, 1.0f, 0.0f); // Top face
-        }
-        else if (info.t == tZMin) {
-            info.normal = glm::vec3(0.0f, 0.0f, -1.0f); // Front face
-        }
-        else {
-            info.normal = glm::vec3(0.0f, 0.0f, 1.0f); // Back face
-        }
-
-        return true;
+    bool hit_sphere(const glm::vec3& center, double radius, const ray& r) {
+        glm::vec3 oc = r.origin() - center;
+        auto a = dot(r.direction(), r.direction());
+        auto b = 2.0 * dot(oc, r.direction());
+        auto c = dot(oc, oc) - radius * radius;
+        auto discriminant = b * b - 4 * a * c;
+        return (discriminant >= 0);
     }
 
     colorRGB shade(const HitInfo & info) {
@@ -115,11 +37,14 @@
     }
 
     void saveAsPPM(const std::vector<std::vector<colorRGB>>&pixelData, int width, int height, const char* filename) {
-        std::cout << "Saving image..."  << std::endl;
+        std::cout << "Saving image.";
         std::ofstream img(filename);
         img << "P3" << std::endl;
         img << width << " " << height << std::endl;
         img << "255" << std::endl;
+
+        int dotSegment = floor(height / 8);
+        int dotCounter = 0;
 
         for (int i = 0; i < height; ++i) {
             for (int j = 0; j < width; ++j) {
@@ -129,8 +54,19 @@
 
                 img << r << " " << g << " " << b << std::endl;
             }
+
+            dotCounter++;
+            if (dotCounter >= dotSegment)
+            {
+                if (i <= height - dotSegment)
+                {
+                    std::cout << ".";
+                    dotCounter = 0;
+                }
+            }
         }
 
+        std::cout << std::endl;
         std::cout << "Image saved as '" << filename << "'" << std::endl;
     }
 
@@ -143,12 +79,12 @@
         glm::vec3 backgroundColor(0.0f);
         //std::vector<std::vector<glm::vec3>> pixelData(imageHeight, std::vector<glm::vec3>(imageWidth, backgroundColor));
 
-        glm::vec3 boxMin(0.2f, 0.2f, 0.2f);
-        glm::vec3 boxMax(0.8f, 0.8f, 0.8f);
+        glm::vec3 sphereCenter(0.0f, 0.0f, 0.0f);
+        double sphereRadius = 0.2f;
 
         // Camera setup
-        glm::vec3 cameraPosition(0.0f, 0.0f, -2.0f);  // Position of the camera
-        glm::vec3 cameraTarget(0.5f, 0.5f, 0.5f);    // Point the camera is looking at
+        glm::vec3 cameraPosition(0.0f, 0.0f, -50.0f);  // Position of the camera
+        glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);    // Point the camera is looking at
         glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);        // Up direction of the camera
         camera cam = camera(imageWidth, imageHeight, cameraPosition, cameraTarget, cameraUp);
 
@@ -173,10 +109,8 @@
 
                 // Perform ray-box intersection and shading as described in the previous example
                 HitInfo hitInfo;
-                if (intersectBox(ray, boxMin, boxMax, hitInfo)) {
-                    cam.pixels[j][i] = shade(hitInfo);
-                }
-
+                hitInfo.hit = hit_sphere(sphereCenter, sphereRadius, ray);
+                cam.pixels[j][i] = shade(hitInfo);
 
                 //// Calculate the pixel center in world space
                 //glm::vec3 pixel_center = cam.cameraPosition + glm::vec3(i, j, 0);
@@ -197,7 +131,7 @@
 
         std::clog << "\rDone.                 \n";
         
-        saveAsPPM(cam.pixels, imageWidth, imageHeight, "red_box.ppm");
+        saveAsPPM(cam.pixels, imageWidth, imageHeight, "circle_red.ppm");
 
         return 0;
     }
